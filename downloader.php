246e24fe31a2bfa79990e9528e8ac15339cdffc2
spicydog <spicydog@gmail.com>
2017-04-03 20:46:04 +0700

    Add pushbullet notification

--- downloader.php
+++ downloader.php
@@ -138,13 +138,16 @@ for ($i = 0; $i < DOWNLOAD_LIMIT; $i++) {
 
       unset($result);
       $has_update = true;
-      echo "Success\n";
+
       $success = true;
+      echo "Success\n";
 
       // Remove cache
       clear_cache($job);
 
       $next_job = get_next_job($job);
+
+      // Break from retry loop
       break;
     } else {
       echo "Fail\n";
@@ -165,9 +168,36 @@ for ($i = 0; $i < DOWNLOAD_LIMIT; $i++) {
   }
 }
 
+function notify_pushbullet($job) {
+  require_once('view.php');
+  $message['type'] = 'link';
+  $message['title'] = 'Manga ' . convert_name($job['name']) . ' has updated';
+  $message['body'] = 'Check it out and have fun!!';
+  $message['url'] = PUBLIC_URL . 'index.php?source='. $job['crawler']
+    . '&name=' . $job['name']
+    . '&chapter=' . $job['chapter'];
+
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL,"https://api.pushbullet.com/v2/pushes");
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($message));  //Post Fields
+
+  $headers = array();
+  $headers[] = 'Access-Token: ' . PUSHBULLET_ACCESS_TOKEN;
+  $headers[] = 'Content-Type: application/json';
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+  curl_exec($ch);
+  curl_close ($ch);
+}
+
 if ($has_update) {
-  // TODO push notification
   echo sprintf("There are update for %s\n", $job['name']);
+  if (NOTIFY_VIA_PUSHBULLET) {
+    echo sprintf("Sending pushbullet notification\n");
+    notify_pushbullet($job);
+  }
 } else {
   echo sprintf("No update for %s\n", $job['name']);
 }
