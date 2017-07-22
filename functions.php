abf99ff781d0f4330c4a67e1efa85ef6eaaa7810
spicydog <spicydog@gmail.com>
2017-07-23 01:18:47 +0700

    Move functions from index to functions.php

--- functions.php
+++ functions.php
@@ -90,3 +90,87 @@ function convert_name($id) {
   $name = ucwords($name);
   return $name;
 }
+
+
+function filter_dir($names) {
+  $results = [];
+  foreach ($names as $name) {
+    if ($name != 'index.html' && $name != 'index.json' && substr($name, 0, 1) != '.') {
+      $results[] = $name;
+    }
+  }
+  return $results;
+}
+
+function get_crawlers() {
+  $crawlers = [];
+  $file_path = FILES_DIR;
+  if (file_exists($file_path)) {
+    $content = @file_get_contents(FILES_DIR . 'index.json');
+    if (strlen($content) > 2) {
+      $crawlers = json_decode($content, true);
+    } else {
+      $crawlers = filter_dir(scandir(FILES_DIR));
+      sort($crawlers);
+      file_put_contents(FILES_DIR . 'index.json', json_encode($crawlers));
+    }
+  }
+  return $crawlers;
+}
+
+function get_names($crawler) {
+  $names = [];
+  $crawler_path = FILES_DIR . '/' . $crawler . '/';
+  if (file_exists($crawler_path)) {
+    $content = @file_get_contents($crawler_path . 'index.json');
+    if (strlen($content) > 2) {
+      $names = json_decode($content, true);
+    } else {
+      $names = filter_dir(scandir($crawler_path));
+      sort($names);
+      file_put_contents($crawler_path . 'index.json', json_encode($names));
+    }
+  }
+  return $names;
+}
+
+function get_chapters($crawler, $name) {
+  $chapters = [];
+  $name_path = FILES_DIR . '/' . $crawler . '/' . $name . '/';
+  if (file_exists($name_path)) {
+    $content = @file_get_contents($name_path . 'index.json');
+    if (strlen($content) > 2) {
+      $chapters = json_decode($content, true);
+    } else {
+      $chapters = filter_dir(scandir($name_path));
+      sort($chapters);
+      file_put_contents($name_path . 'index.json', json_encode($chapters));
+    }
+  }
+  return $chapters;
+}
+
+function get_images($crawler, $name, $chapter) {
+  $images = [];
+  $chapter_path = FILES_DIR . '/' . $crawler . '/' . $name . '/' . $chapter . '/';
+  if (file_exists($chapter_path)) {
+    $content = @file_get_contents($chapter_path . 'index.json');
+    if (strlen($content) > 2 && false) {
+      $images = json_decode($content, true);
+    } else {
+      $images = filter_dir(scandir($chapter_path));
+      sort($images, SORT_NUMERIC);
+      file_put_contents($chapter_path . 'index.json', json_encode($images));
+    }
+  }
+  return $images;
+}
+
+function generate_image_urls($crawler, $name, $chapter, $images) {
+  $urls = [];
+  $chapter_path = FILES_DIR . $crawler . '/' . $name . '/' . $chapter . '/';
+  foreach ($images as $image) {
+    $urls[] = $chapter_path . $image;
+  }
+  return $urls;
+}
