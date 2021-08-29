99a6f06e06d7d785bcc7de24f70a9669a028997c
spicydog <spicydog@gmail.com>
2021-08-29 13:22:48 +0700

    Refactor kissmanga

--- crawlers/kissmanga.php
+++ crawlers/kissmanga.php
@@ -1,23 +1,18 @@
 <?php
 
-function get_inner_string($string, $begin, $end) {
-  if($begin === 0)
-    return substr( $string, 0, strpos($string, $end) );
-  if($end === 0)
-    return substr($string, strpos($string, $begin) + strlen($begin));
+require_once 'utils.inc';
 
-  $string = ' ' . $string;
-  $init = strpos($string, $begin);
-
-  if ($init == 0)
-    return '';
-
-  $init += strlen($begin);
-  $len = strpos($string, $end, $init) - $init;
-
-  return substr($string, $init, $len);
-}
-
+// Code for test
+// download([
+//   'name' => 'manga-cg980189',
+//   'chapter' => '1',
+//   'page' => 1,
+// ]);
+// download([
+//   'name' => 'read_one_piece_manga_online_free4',
+//   'chapter' => '996',
+//   'page' => 1,
+// ]);
 
 function download($job) {
 
@@ -32,25 +27,21 @@ function download($job) {
   $page = $job['page'];
 
   $url = "https://kissmanga.org/chapter/$name/chapter-$chapter";
-
-  $html = file_get_contents($url);
+  $html = @file_get_contents($url);
+  if (strlen($html) === 0) {
+    $url = "https://kissmanga.org/chapter/$name/chapter_$chapter";
+    $html = @file_get_contents($url);
+  }
 
   $content = get_inner_string($html, 'centerDivVideo', '</div>');
 
   $imgs = explode('src=', $content);
-  $imgs = array_filter($imgs, function($img) {
-  	return strpos($img, 'https://') !== false;
-  });
-  $imgs = array_map(function($img) {
-  	return get_inner_string($img, '"', '"');
-  }, $imgs);
+  $imgs = array_filter_extensions($imgs, ['png', 'jpg', 'jpeg']);
+  $imgs = array_map_double_qoutes($imgs);
   $imgs = array_values($imgs);
 
-
   $job['last_page'] = count($imgs);
-
   $image_url = $imgs[$page-1];
-
   if (strlen($image_url) > 10) {
     $image = file_get_contents($image_url);
     if (strlen($image) > 100) {
