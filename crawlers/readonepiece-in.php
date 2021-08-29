5c415b1153a7525c80c87bfa84ceed256f6588c8
spicydog <spicydog@gmail.com>
2021-08-29 13:14:50 +0700

    Refactor readonepiece.in

--- crawlers/readonepiece-in.php
+++ crawlers/readonepiece-in.php
@@ -1,22 +1,6 @@
 <?php
 
-function get_inner_string($string, $begin, $end) {
-  if($begin === 0)
-    return substr( $string, 0, strpos($string, $end) );
-  if($end === 0)
-    return substr($string, strpos($string, $begin) + strlen($begin));
-
-  $string = ' ' . $string;
-  $init = strpos($string, $begin);
-
-  if ($init == 0)
-    return '';
-
-  $init += strlen($begin);
-  $len = strpos($string, $end, $init) - $init;
-
-  return substr($string, $init, $len);
-}
+require_once 'utils.inc';
 
 // Code for test
 // download([
@@ -38,28 +22,17 @@ function download($job) {
   $page = $job['page'];
 
   $url = "https://readonepiece.in/manga/$name-chapter-$chapter/";
-
   $html = file_get_contents($url);
-
   $content = get_inner_string($html, 'margin-top: 0px; margin-bottom: 0px;', '</body>');
 
   $imgs = explode('src=', $content);
-  $imgs = array_filter($imgs, function($img) {
-    return strpos($img, 'one-piece-') > 0;
-  });
-  $imgs = array_map(function($img) {
-    return get_inner_string($img, '"', '"');
-  }, $imgs);
-  $imgs = array_filter($imgs, function($img) {
-    return strpos($img, 'one-piece-') > 0;
-  });
-
+  $imgs = array_map_double_qoutes($imgs);
+  $imgs = array_filter_contain_neddle($imgs, 'one-piece-');
+  $imgs = array_filter_extensions($imgs, ['png', 'jpg', 'jpeg']);
   $imgs = array_values($imgs);
 
   $job['last_page'] = count($imgs);
-
   $image_url = $imgs[$page-1];
-
   if (strlen($image_url) > 10) {
     $image = file_get_contents($image_url);
     if (strlen($image) > 100) {
